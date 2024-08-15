2043168d0fcb3dac90d75f63f480c4fe653a2907
Marcos Paulo Silva <118779266+silvamarcospaulo@users.noreply.github.com>
2024-08-15 18:24:26 -0300

    Delete Landing Page directory

--- Landing Page/assets/js/menuHamburguer.js
+++
@@ -1,4 +0,0 @@
-const menuHamburguer = document.querySelector(".menu-hamburguer");
-const nav = document.querySelector(".nav");
-
-menuHamburguer.addEventListener("click", () => nav.classList.toggle("nav-active"));
