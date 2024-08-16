3b82a390367dc86104e27b5ab99fa5212569bf1a
Marcos Paulo <smpmarcospaulo@icloud.com>
2024-08-16 20:09:34 -0300

    Configuração do darkmode automático, menu sanduíche, switch buttom

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,7 @@
 const menuHamburguer = document.querySelector(".menu");
 const nav = document.querySelector(".nav");
+const rotateMenu = document.querySelector(".menu");
 
 menuHamburguer.addEventListener("click", () => nav.classList.toggle("nav-active"));
+
+rotateMenu.addEventListener("click", () => nav.classList.toggle("change"))
