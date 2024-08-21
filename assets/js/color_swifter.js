f95ffdcba9f721966f9541a7085892534ad149ee
Marcos Paulo <smpmarcospaulo@icloud.com>
2024-08-20 22:40:12 -0300

    Ajustando color_swift

--- assets/js/color_swifter.js
+++ assets/js/color_swifter.js
@@ -1,6 +1,19 @@
 const html = document.querySelector('html');
 const checkbox = document.querySelector('#switch');
 
+
+function applyDeviceTheme() {
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (prefersDarkScheme) {
+        checkbox.checked = false;
+    } else {
+        checkbox.checked = true;
+    }
+}
+
+applyDeviceTheme();
+
 checkbox.addEventListener('click', function () {
-    html.classList.toggle('dark-mode');
+        html.classList.toggle('dark-mode');
 });
